57af1802dcc4fcda737da4f78db4b6543c317289
xDamneDx <xdamnedx@gmail.com>
2023-04-05 11:21:51 +0300

    divide interface into components

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,36 @@
 import logo from "./assets/logo.png";
 
-function App() {
+export default function App() {
   return (
-    <header className="header">
-      <div className="logo">
-        <img src={logo} height="68" width="68" alt="Today I Learned Logo" />
-        <h1>Today I Learned</h1>
-      </div>
+    <>
+      {/* HEADER */}
+      <header className="header">
+        <div className="logo">
+          <img src={logo} height="68" width="68" alt="Today I Learned Logo" />
+          <h1>Today I Learned</h1>
+        </div>
 
-      <button className="btn btn-large btn-open">Share a fact</button>
-    </header>
+        <button className="btn btn-large btn-open">Share a fact</button>
+      </header>
+
+      <NewFactForm />
+
+      <main className="main">
+        <CategoryFilter />
+        <FactList />
+      </main>
+    </>
   );
 }
 
-export default App;
+function NewFactForm() {
+  return <form className="fact-form">NewFactForm</form>;
+}
+
+function CategoryFilter() {
+  return <aside>CategoryFilter</aside>;
+}
+
+function FactList() {
+  return <section>FactList</section>;
+}
