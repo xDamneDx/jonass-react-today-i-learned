3ef5f239231a4625c7c1e41435aa4c2b76ec8b46
xDamneDx <xdamnedx@gmail.com>
2023-04-05 12:29:36 +0300

    render list of categories

--- src/App.jsx
+++ src/App.jsx
@@ -73,7 +73,25 @@ function NewFactForm() {
 }
 
 function CategoryFilter() {
-  return <aside>CategoryFilter</aside>;
+  return (
+    <aside>
+      <ul>
+        <li className="category">
+          <button className="btn btn-all-categories">All</button>
+        </li>
+        {CATEGORIES.map((category) => (
+          <li key={category.name} className="category">
+            <button
+              className="btn btn-category"
+              style={{ backgroundColor: category.color }}
+            >
+              {category.name}
+            </button>
+          </li>
+        ))}
+      </ul>
+    </aside>
+  );
 }
 
 function FactList() {
